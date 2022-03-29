1f906fc3d123da3f764621305871ef4b85678184
Wxy <Wxy@126.com>
2022-03-29 17:12:03 +0800

    hot-fix commit1

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,5 +2,8 @@ public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("Hello git");
+        System.out.println("Hello git2");
+        System.out.println("Hello git3");
+        System.out.println("Hello git4");
     }
 }
