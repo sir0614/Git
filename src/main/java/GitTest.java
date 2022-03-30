e4bf83276b6bbb40e2bfa925701b8a5737940d5d
sir0614 <101536462+sir0614@users.noreply.github.com>
2022-03-30 08:41:12 +0800

    Update GitTest.java

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("master git5");
         System.out.println("hot-fix git5");
         System.out.println("push test");
+        System.out.println("github test");            
     }
 }
